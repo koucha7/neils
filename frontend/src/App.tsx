6e5863d82e8cab195e2445f9120045331a43ebdb
koucha <koucha7@outlook.com>
2025-06-12 02:37:33 +0900

    deploy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,6 @@ function App() {
     <div className="min-h-screen bg-gradient-to-br from-purple-500 to-indigo-600 flex flex-col justify-center items-center p-4">
       <header className="text-center text-white mb-12">
         <h1 className="text-5xl font-extrabold tracking-tight">NailMomo</h1>
-        <p className="mt-3 text-xl font-light">あなたの美しい指先をサポート</p>
       </header>
 
       <main className="flex flex-col md:flex-row space-y-6 md:space-y-0 md:space-x-8">
@@ -23,13 +22,6 @@ function App() {
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
           予約を確認・キャンセル
-        </Link>
-        <Link to="/admin" className="bg-white text-gray-700 px-8 py-4 rounded-lg shadow-xl hover:shadow-2xl transform hover:-translate-y-1 transition duration-300 ease-in-out flex items-center justify-center text-xl font-semibold">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.324 2.572-1.065z" />
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-          </svg>
-          管理者画面
         </Link>
       </main>
     </div>
