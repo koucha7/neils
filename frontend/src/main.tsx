10c89bb9b4117ab92087f8cd162584a6f0ec7835
koucha <koucha7@outlook.com>
2025-06-12 16:15:07 +0900

    deploy

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 
